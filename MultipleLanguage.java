5fe4dc540be53eadf6eccf7a41a748e522006978
gowthami-krishnan <108142949+gowthami-krishnan@users.noreply.github.com>
2022-07-01 14:14:50 +0530

    Add files via upload

---
+++ MultipleLanguage.java
@@ -0,0 +1,12 @@
+package Week3;
+
+public abstract class MultipleLanguage implements Language,TestTool {
+       
+	     
+	     public void Python() {
+	    	 System.out.println("Python is also a language" );
+	     }
+	     
+	     public abstract String ruby(String str);
+	     
+}
